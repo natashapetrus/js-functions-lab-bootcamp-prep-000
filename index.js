7e257c7733aa3e67f37921ec1af81a27b0ddc255
Natasha Petrus <hello@natashapetrus.com>
2017-09-02 06:02:54 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,15 @@
+function happyHolidays(){
+  return "Happy holidays!"
+}
+
+function happyHolidaysTo(name){
+  return `Happy holidays, ${name}!`
+}
+
+function happyHolidayTo(holiday, name){
+  return `Happy ${holiday}, ${name}!`
+}
+
+function holidayCountdown(days, holiday){
+  `It's ${days} days until ${holiday}!`
+}
